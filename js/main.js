62c0960bdbe09129f0494b54c9751b42c58a0f75
MrDh0nt <3v3rtdhont@gmail.com>
2016-10-20 21:17:37 +0200

    working on th arrow input a little more

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var testerrr = 0;
+var loop;
+var keypresss;
+function init(){
+    loop = setInterval(gameLoop(), (60/1000));
+}
 function make(width, height, id){
     var thisobject = document.getElementById(id);
     document.getElementById(id).style.width = width;
@@ -7,11 +13,14 @@ function make(width, height, id){
     document.getElementById(id).style.top = "0px";
 }
 function gameLoop(){
-    var exit = false;
-    while(!exit){
-        setInterval(function(){alert('hello')}, 3000);
+    document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 38:
+            alert('up');
+            break;
+        case 40:
+            alert('down');
+            break;
     }
-}
-function isKeyPressed(test){
-    console.log('yeps: ' + test);
+};
 }
