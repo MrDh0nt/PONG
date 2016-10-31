79510cf6e052aa065af24c41536efeba2d7f6457
MrDh0nt <3v3rtdhont@gmail.com>
2016-10-31 11:13:52 +0100

    now the ball is moving, I am in the process of making the paddles move

--- js/main.js
+++ js/main.js
@@ -1,26 +1,66 @@
-var testerrr = 0;
-var loop;
-var keypresss;
-function make(width, height, id){
-    var thisobject = document.getElementById(id);
-    document.getElementById(id).style.width = width;
-    document.getElementById(id).style.height = height;
-    document.getElementById(id).style.position = "absolute";
-    document.getElementById(id).style.left = "0px";
-    document.getElementById(id).style.top = "0px";
-}
-function gameLoop(){
-    document.onkeydown = function(e) {
-    switch (e.keyCode) {
-        case 38:
-            alert('up');
-            break;
-        case 40:
-            alert('down');
-            break;
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 400;
+var barwidth = 15;
+var barheight = 75;
+var white = "#FFF";
+var pX = 0;
+var pY = ((canvas.height / 2) - (barheight / 2));
+var cX = canvas.width - barwidth;
+var cY = ((canvas.height / 2) - (barheight / 2));
+var ballr = 20;
+var ballx = 0;
+var bally = ((canvas.height / 2) - (ballr / 2));
+var xchange = 7;
+var ychange = 7;
+var upPress = false;
+var downPress = false;
+var pYchange = 7;
+var bodyHtml = document.getElementById("myBody");
+
+
+var draw = {
+	rectangle: function (x, y, w, h, color) {
+		ctx.beginPath();
+		ctx.rect(x, y, w, h);
+		ctx.fillStyle = color;
+		ctx.fill();
+		ctx.closePath();
+	},
+    clearScreen: function () {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.closePath();
     }
+	
 };
+
+function gameLoop() {
+    draw.clearScreen();
+	//code here...
+    //1 detection
+    var x = event.keyCode;
+    if(x){
+        console.log(x);
+    }
+    //2 logic
+    if(ballx < 0 || ballx + ballr > canvas.width) {
+        xchange = -xchange;
+    }
+    if(bally < 0 || bally + ballr > canvas.height) {
+        ychange = -ychange;
+    }
+    //3 drawing
+    draw.rectangle(pX, pY, barwidth, barheight, white);
+    draw.rectangle(cX, cY, barwidth, barheight, white);
+    draw.rectangle(ballx, bally, ballr, ballr, white);
+    for(var i = 0; i < canvas.height; i = i + 20){
+        draw.rectangle((canvas.width / 2) - 2.5, i, 5, 13);
+    }
+    
+    ballx += xchange;
+    bally += ychange;
 }
-function init() {
-    loop = setInterval(gameLoop(), (60 / 1000));
-}
+setInterval(gameLoop, 40);
